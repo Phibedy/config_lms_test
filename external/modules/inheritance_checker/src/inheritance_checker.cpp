b6f5a3f88dbd6370d92194c13a994580d9ea896a
phibedy <philipp.schmette@gmx.de>
2015-11-13 23:32:31 +0100

    update.....

--- external/modules/inheritance_checker/src/inheritance_checker.cpp
+++ external/modules/inheritance_checker/src/inheritance_checker.cpp
@@ -9,6 +9,7 @@
 class Inheritance{
 public:
     virtual void callMe()  = 0;
+    virtual ~Inheritance(){}
 
 };
 
@@ -41,13 +42,21 @@ class vertex2{
 public:
     virtual ~vertex2() {}
 };
-typedef vertex2<float> vertex2f;
+
+class vertex2Dump{
+    public:
+    virtual ~vertex2Dump(){}
+};
+
+typedef vertex2Dump vertex2f;
+//typedef vertex2<float> vertex2f;
 
 class A0{
 public:
+    virtual ~A0(){}
 };
-
-class A: public vertex2f, public A0, public Inheritance{
+///removing vertex2f causes the programm to work like it should!
+class A: public A0, public Inheritance{
 public:
     void callMe() override{
         std::cout << "IN A "<<std::endl;
@@ -67,40 +76,6 @@ bool InheritanceChecker::initialize() {
     //works with clang but throws a segfault in gcc 4.9
     std::cout << "2."<<std::endl;
     static_cast<T*>(main->get())->callMe();
-
-
-    //if vertex2f is removed, callMe is called in both cases and gcc doesn't crash!
-
-
-
-    //lms::WriteDataChannel<T> d  = datamanager()->writeChannel<T>(this,"A");
-
-
-    //trying to call the method isSubType overriden in A, tested with gcc 4.9 and clang 3.4
-    /*
-    //works
-    logger.info("0 YES:");
-    d.get()->isSubType(12);
-
-    //doesn't work
-    logger.info("1 NO:");
-    static_cast<lms::Inheritance*>(d.m_internal->main->get())->isSubType(12);
-
-    //works with clang but throws a segfault in gcc 4.9
-    logger.info("3 (Yes)");
-    static_cast<T*>(d.m_internal->main->get())->isSubType(12); //same as d.get() but causes a seqfault with gcc 4.9, but works with clang
-
-    //doesn't work
-    logger.info("4. NO");
-    std::unique_ptr<ObjectBase> main;
-    main.reset(new Object<T>);
-    lms::Inheritance *inh = static_cast<lms::Inheritance*>(main->get());
-    inh->isSubType(12);
-
-    //works with clang but throws a segfault in gcc 4.9
-    logger.info("5. (Yes)");
-    static_cast<T*>(main->get())->isSubType(12);
-    */
 
     return true;
 }
